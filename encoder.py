b9fbac4b05d841d81097c6a17ee32ba9eec68150
Natalie Kocsis <98858315+nkocsis27@users.noreply.github.com>
2023-08-05 17:50:05 -0400

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,147 @@
+import struct
+
+
+def encoder_IADD32I(cc, x, sat, three_for_po, neg_a, src, dst, imm):
+    opcode = 0b0001110
+
+    # imm = 0b101011001
+    # imm >> 27 #from 20 for 32 bits for all imm
+    encode = opcode << 57 #opcode - 64 is the left shift
+    encode |= (neg_a & 1) << 56
+    encode |= (sat & 1) << 54 
+    encode |= (x & 1) << 53
+    encode |= (cc & 1) << 52
+    encode |= (three_for_po & 3) << 55 
+    encode |= imm << 20
+    
+    encode |= (dst & 255) #ensures width is within 255, do 2^bit width
+    encode |= (src & 255) << 8
+    
+    # print(encode)
+    return encode
+
+def encoder_FADD32I(ftz, neg_b, abs_a, cc, neg_a, abs_b, dest_reg, src_a, imm):
+    opcode = 0b000010 
+    #NOTE for imm for floats work differently, so just use all 0's
+    e = opcode << 58
+    e |= imm << 20
+    e |= (abs_b & 1) << 57
+    e |= (neg_a & 1) << 56
+    e |= (ftz & 1) << 55
+    e |= (abs_a & 1) << 54
+    e |= (neg_b & 1) << 53
+    e |= (cc & 1) << 52
+    
+    e |= (dest_reg & 255) 
+    e |= (src_a & 255) << 8
+    
+    return e
+
+def encoder_DADD_reg(fp_rounding, neg_b, abs_a, cc, neg_a, abs_b, dest_reg, src_a_reg):
+    opcode = 0b0101110001110
+    e = opcode << 51 
+    e |= (fp_rounding & 3) << 39
+    e |= (neg_b & 1) << 45
+    e |= (abs_a & 1) << 46
+    e |= (cc & 1) << 47
+    e |= (neg_a & 1) << 48
+    e |= (abs_b & 1) << 49
+    
+    e |= (dest_reg & 255) 
+    e |= (src_a_reg & 255) << 8
+    
+    return e
+
+def encoder_LOP32I(bit_op, x, cc, inv_a, inv_b, dest_reg, src_reg, imm):
+    opcode = 0b000001
+
+    e = opcode << 58
+    e |= imm << 20
+    e |= (bit_op & 3) << 53
+    e |= (x & 1) << 57
+    e |= (cc & 1) << 52
+    e |= (inv_a & 1) << 55
+    e |= (inv_b & 1) << 56
+    
+    e |= (dest_reg & 255) 
+    e |= (src_reg & 255) << 8
+    
+    return e
+    
+def encoder_ISETP_reg(bop_pred, neg_bop_pred, x, bop, is_signed, compare_op, dest_pred_b, dest_pred_a, src_reg_a):
+    opcode = 0b010110110110
+    e = opcode << 52
+    e |= (bop_pred & 7) << 39
+    e |= (neg_bop_pred & 1) << 42
+    e |= (x & 1) << 43
+    e |= (bop & 3) << 45
+    e |= (is_signed & 1) << 48
+    e |= (compare_op & 7) << 49
+    
+    e |= (dest_pred_a & 7) << 3
+    e |= (dest_pred_b & 7)
+    e |= (src_reg_a & 255) << 8
+    
+    return e
+
+def encoder_I2F_reg(float_format, int_format, is_signed, fp_rounding, selector, cc, neg, abs, reg, dest_reg) :
+    opcode = 0b0101110010111
+    e = opcode << 51
+    e |= (float_format & 3) << 8
+    e |= (int_format & 3) << 10
+    e |= (is_signed & 1) << 13
+    e |= (fp_rounding & 3) << 39
+    e |= (selector & 3) << 41
+    e |= (cc & 1) << 47
+    e |= (neg & 1) << 45
+    e |= (abs & 1) << 49
+    e |= (reg & 255) << 20
+    
+    e |= (dest_reg & 255)
+    
+    return e
+    
+
+#0b101011001
+u64a = encoder_IADD32I(0,1,0,1,1,0,0,0b101011001)
+print(hex(u64a))
+
+u64b = encoder_FADD32I(0,1,0,0,1,1,0,0,0b000000)
+print(hex(u64b))
+
+u64c = encoder_DADD_reg(5,0,0,1,0,0,0,0)
+print(hex(u64c))
+
+u64d = encoder_LOP32I(0,1,0,0,0,0,0,0b000)
+print(hex(u64d))
+
+u64e = encoder_ISETP_reg(0,1,0,0,1,0,0,0,0)
+print(hex(u64e))
+
+u64f = encoder_I2F_reg(2,2,1,0,0,0,0,1,0,0)
+print(hex(u64f))
+
+
+#turns into string?
+
+sencodeA = struct.pack('Q',u64a) 
+sencodeB = struct.pack('Q',u64b) 
+sencodeC = struct.pack('Q',u64c) 
+sencodeD = struct.pack('Q',u64d) 
+sencodeE = struct.pack('Q',u64e) 
+sencodeF = struct.pack('Q',u64f) 
+
+
+
+
+#write to file
+f = open("encodes.txt", "w")
+f.write(str(sencodeA)+ '\n')
+f.write(str(sencodeB)+ '\n')
+f.write(str(sencodeC)+ '\n')
+f.write(str(sencodeD)+ '\n')
+f.write(str(sencodeE)+ '\n')
+f.write(str(sencodeF)+ '\n')
+f.close()
+
+
---
+++ nvdisasm_python.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+import sys
+
+# this is some random 4 instruction sequence that I grabbed from a real disassembly, 
+# though it was for Pascal.
+# Make sure it works for maxwell, or try to get a 4 instruction sequence using cuobjdump 
+# on some binary (we can chat briefly over email or zoom if you need help on this)
+INITIAL = bytearray(b'\xf2\x07!\xfe\x00\xbc\x1f\x00\x00\x02G\x00\x00\x88\x10\\\x00\x00\x07\x01\x00\x00@\xe2\x00\x0f\x07\x00\x00\x00\xb0P')
+working_text = INITIAL.copy()
+
+encode = bytearray(b'\x00*\x00\x00\x00\x00\xba\\')
+
+# edit parts of working_text here
+# first 64 bytes correspond to the third instruction, so stick to this
+# e.g., you can edit bytes like so:
+# for i in range(8):  # have to edit all 8 bytes of the encoding
+#     working_set[i] = x  # here, x is the corresponding part of the instruction encoding.
+for i in range(8):
+    working_text[i+8] = encode[i]
+
+with open('tmp_bin', 'wb') as outFile:
+    outFile.write(working_text)
+
+sp_args = ["nvdisasm", "-b", "SM53", "tmp_bin", "-hex"]
+try:
+    res = subprocess.run(sp_args, capture_output=True, check=True)
+    txt = res.stdout.decode('utf-8')#.split('\n')[2]
+    f = open("instructs","w")
+    f.write(txt)
+    # print out whichever instruction you modify, change 'n' to whichever is appropriate here.
+    print(txt)
+    #print("nvdisasm output:", res.stdout.decode('utf-8').split('\n')[1])
+except subprocess.CalledProcessError:
+    print("nvdisasm error, likely due to an invalid instruction encoding", file=sys.stderr)
