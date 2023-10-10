6548abb870c2677cded9d8765e343d8f113f061c
Natalie Kocsis <98858315+nkocsis27@users.noreply.github.com>
2023-10-10 11:45:25 -0400

    Update validator.c

--- validator.c
+++ validator.c
@@ -16,6 +16,12 @@ uint64_t IADD64 (uint64_t a, uint64_t b) {
 }
 
 
+//TODO DADD
+//if either number is NaN (+/- infinity) result is also NaN
+double DADD (double r1, double r2) {
+    return r1 + r2;
+}
+
 
 
 /*
@@ -36,13 +42,10 @@ uint64_t OpIAdd64(uint64_t v1,uint64_t v2) {
 
 //FIXME: floating point type (signed int only up to 32)
 float OpConvertSToF(int32_t signedInt) {
-    //FIXME: cannot cast to float
+    //FIXME: cast to float?
     return (float) signedInt;
 }
 
-float OpFAdd(float op1, float op2) {
-    return op1 + op2;
-}
 
 //smaller bit size than 32 for int type? only 1 or 0, boolean?
 uint32_t OpBitwiseAnd(uint32_t op1, uint32_t op2) {
@@ -52,6 +55,36 @@ uint32_t OpBitwiseAnd(uint32_t op1, uint32_t op2) {
         return 0;
     }
 }
+
+//TODO spir-v implementation of double addition
+//Converts from double to int and performs arithmetic
+
+//FIXME allignment wrong? 64 bit int wrong?
+//memcpy function?
+union doubleToInt64 {
+    double f;
+    u_int64_t i;
+} v; //variable v of type doubleToInt64
+
+u_int64_t yuzuLowered(double d) {
+//FIXME missing details 
+    v.f = d;
+    printf("%llu\n",v.i);
+    return v.i;
+
+}
+
+double SprivDoubleAdd (double a, double b) {
+    return (double)OpIAdd64(yuzuLowered(a), yuzuLowered(b));
+}
+
+
+
+
+// //32 bit float only
+// float OpDADD(double op1, double op2) {
+//     return (float)op1 + (float)op2;
+// }
 
 
 
@@ -78,37 +111,55 @@ int IAdd_X(int a, int b, bool x) {
     return 0;
 }
 
+//TODO FUZZER
+int LLVMFuzzerTestOneInput(double a, double b) {
+  SprivDoubleAdd(a, b);
+  return 0;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    uint32_t R2, R1 = 15, R3 = 5;
-    R2 = IADD32 (R1,R3);
-    printf("Maxwell IADD: %u\n", R2);
+    // uint32_t R2, R1 = 15, R3 = 5;
+    // R2 = IADD32 (R1,R3);
+    // printf("Maxwell IADD: %u\n", R2);
 
-    uint64_t X2, X1 = 15, X3 = 5;
-    X2 = OpIAdd64 (X1,X3);
-    printf("SPRI-V OpIAdd: %llu\n", X2);
+    // uint64_t X2, X1 = 15, X3 = 5;
+    // X2 = OpIAdd64 (X1,X3);
+    // printf("SPRI-V OpIAdd: %llu\n", X2);
 
-    float result;
-    uint32_t i = 5;
-    result = OpConvertSToF(i);
-    printf("OpConvertSToF: %u\n", result);
+    // float result;
+    // uint32_t i = 5;
+    // result = OpConvertSToF(i);
+    // printf("OpConvertSToF: %f\n", result);
 
-    float opResult, op1 = 1, op2 = 4;
-    opResult = OpFAdd(op1, op2);
-    printf("OpFAdd: %u\n", opResult);
+    // float opResult, op1 = 1, op2 = 4;
+    // opResult = OpFAdd(op1, op2);
+    // printf("OpFAdd: %f\n", opResult);
 
-    uint32_t x, a = 1, b = 1;
-    x = OpBitwiseAnd(a,b);
-    printf("OpBitwiseAnd: %u\n", x);
-
-
+    // uint32_t x, a = 1, b = 1;
+    // x = OpBitwiseAnd(a,b);
+    // printf("OpBitwiseAnd: %u\n", x);
 
 
+    double X, Y, b2 = 7.4, b3 = 2.0;
+    X = DADD(b2, b3);
+    printf("DADD: %f\n", X);
 
-    assert (R1 == X1);
-    assert (R3 == X3); //FIXME assertion fails with signed numbers 
-    assert (R2 == X2); 
+    Y = SprivDoubleAdd(b2,b3);
+    printf("SpirV Double Add: %f\n", Y);
+
+
+    // assert (R1 == X1);
+    // assert (R3 == X3); 
+    // assert (R2 == X2); 
+    // assert (b1 == c1);
+    //\X is DADD and Y is Spir-v interpretation of double addition.
+    
+    
+
+
+    assert(X == Y);
 
     
     return 0;
